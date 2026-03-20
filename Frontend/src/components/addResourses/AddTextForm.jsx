12bb38ef17c10c7da02f718e9f243e05f5386b85
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-03-20 14:26:51 -0600

    feat: add validation to word and text forms

--- Frontend/src/components/addResourses/AddTextForm.jsx
+++ Frontend/src/components/addResourses/AddTextForm.jsx
@@ -4,6 +4,7 @@ import { postText, putText } from "../../api/textService"
 export default function AddTextForm({ style, setTexts, editingText, setEditingText, fetchTexts, pageSize, texts }) {
     const isEditing = editingText !== null
     const submitBtnText = isEditing ? "Edit text" : "Add text"
+    const [errors, setErrors] = useState({})
 
     const [formValues, setFormValues] = useState({
         title: "",
@@ -20,6 +21,12 @@ export default function AddTextForm({ style, setTexts, editingText, setEditingTe
 
     const handleSubmit = (event) => {
         event.preventDefault()
+
+        const validationErrors = validate()
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors)
+            return
+        }
 
         if (isEditing) {
             editText()
@@ -65,12 +72,27 @@ export default function AddTextForm({ style, setTexts, editingText, setEditingTe
         }
     }, [editingText])
 
+    const validate = () => {
+        const errors = {}
+
+        if (!formValues.text.trim()) {
+            errors.text = "Text must not be empty"
+        }
+        if (!formValues.title.trim()) {
+            errors.title = "Title must not be empty"
+        }
+
+        return errors
+    }
+
     return (
         <form className={style} onSubmit={(event) => handleSubmit(event)}>
             <label htmlFor="title">Title</label>
             <input type="text" id="title" name="title" value={formValues.title} onChange={(event => handleChange(event))} />
+            <p>{errors.title}</p>
             <label htmlFor="text">Text</label>
             <textarea id="text" name="text" value={formValues.text} onChange={(event => handleChange(event))}></textarea>
+            <p>{errors.title}</p>
             <button type="submit">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
