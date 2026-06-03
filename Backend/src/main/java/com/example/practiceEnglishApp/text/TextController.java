28c617d132b53b408ca835ce7784470b2cdfd2e9
Luis Gabriel <luis.gabo.rex@gmail.com>
2026-06-03 07:46:49 -0600

    fix(api): fix text entitiy path

--- Backend/src/main/java/com/example/practiceEnglishApp/text/TextController.java
+++ Backend/src/main/java/com/example/practiceEnglishApp/text/TextController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "api/v1/")
+@RequestMapping(path = "api/v1/text")
 public class TextController {
     private final TextService textService;
 
